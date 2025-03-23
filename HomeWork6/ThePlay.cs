a6fee12cb7fcdb7a1c6356309c766ac21610712e
Mykyta Khomik <n.homick4@gmail.com>
2025-03-23 14:45:00 +0200

    Add project files.

---
+++ HomeWork6/ThePlay.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork6
+{
+    internal class ThePlay : IDisposable
+    {
+        public string Name { get; set; }
+        public string FullName { get; set; }
+        public int Year { get; set; }
+        public string Genre { get; set; }
+
+        public void Dispose()
+        {
+            Console.WriteLine($"The end of {Name}");
+        }
+
+        ~ThePlay()
+        {
+            Dispose();
+        }
+    }
+}
